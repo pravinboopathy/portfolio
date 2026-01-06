5605fa946a4f4f837ebe0f61a489d3c1875e18ea
pravinboopathy <pravinboopathy@gmail.com>
2026-01-06 07:31:39 -0600

    update base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pravinboopathy.github.io',
-  base: '/portfolio',
+  site: 'https://pravinboopathy.com',
+  base: '/',
 
   vite: {
     plugins: [tailwindcss()]
